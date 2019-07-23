8e07928fc42bcdec0598ea16e1b62663076ea3ac
mamariscal <mamariscal@csumb.edu>
2019-07-23 11:45:52 -0400

    Hw1 using Express

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 const express = require("express");
 const app = express();
+const faker = require('faker');
+
+//app.set("view engine", "ejs");
+
+
 app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-  res.render("index.html");
+  res.render("index.ejs");
 });
 
 app.get("/gamecompanies", function(req,res){
-  res.render("gamecompanies.html");
+  res.render("gamecompanies.ejs", {"fakeCompanyName": faker.company.companyName()});
 });
 
 app.get("/populargames", function(req,res){
-  res.render("populargames.html");
+  res.render("populargames.ejs")
 });
 
 app.get("/howto", function(req,res){
-  res.render("howto.html");
+  res.render("howto.ejs", {"fakeProduct": faker.commerce.productName()});
 });
+
+
+
 //server listener
+//app.listen("8081", "0.0.0.0", function(){
+  // console.log("Express Server is Running...")
+//});
+
 app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("Express Server is Running...")
-});
+    console.log("Express Server is Running...")
+ });
